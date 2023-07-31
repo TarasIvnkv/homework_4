1fda5f489c31ceee179ca0cd2d14ffd642abf382
TarasIvnkv <tarasivanikov@gmail.com>
2023-07-31 21:11:40 +0300

    Add potato functional (raw code)

--- script/script.js
+++ script/script.js
@@ -2,6 +2,9 @@ orderPrice = 0;
 doubleCheese = 5;
 hamburger = 10;
 cheeseBurger = 15;
+smallPotato = 10;
+middlePotato = 15;
+bigPotato = 20;
 additive = ``;
 
 bun = prompt(`hamburger or cheeseburger?`);
@@ -17,6 +20,24 @@ if((bun !== `hamburger` && bun !== `cheeseburger`) || !bun){
         }else{
             orderPrice += cheeseBurger;
         }
+    }
+}
+
+potato = confirm(`Would you like potato?`);
+if(potato === true){
+    potatoSize = prompt(`Choose potato size: small/middle/big`)
+    if(
+        potatoSize === `small`
+        && (potatoSize === ``
+        || potatoSize === false 
+        || potatoSize !== `middle` 
+        || potatoSize !== `big`)
+        ){
+        orderPrice += smallPotato;
+    }else if(potatoSize === `middle`){
+        orderPrice += middlePotato;
+    }else if(potatoSize === `big`){
+        orderPrice += bigPotato;
     }
 }
 
